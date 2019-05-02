bde425584655c32d2ed4d197045d2608bfe315a8
foreverxzhh <foreverxzhh@gmail.com>
2019-05-02 13:49:31 +0800

    Capter 2 finish

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Demo1 for 《Android编程权威指南》
 <h3>Capter1 重要知识点</h3>
 1.自定义Toast<br /> 
 2.使用meta-data
+<h3>Capter2 重要知识点</h3>
+1.MVC（Model-View-Controller）模型<br />
--- app/src/main/java/com/hua/geoquiz/QuizActivity.java
+++ app/src/main/java/com/hua/geoquiz/QuizActivity.java
@@ -1,17 +1,27 @@
 package com.hua.geoquiz;
 
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
+    private Question[] mQuestionBank =
+            {
+                    new Question(R.string.question_australia, true),
+                    new Question(R.string.question_oceans, true),
+                    new Question(R.string.question_mideast, false),
+                    new Question(R.string.question_africa, false),
+                    new Question(R.string.question_americas, true),
+                    new Question(R.string.question_asia, true)
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,33 +29,38 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
+        mNextButton = findViewById(R.id.next_button);
+        mQuestionTextView = findViewById(R.id.question_text_view);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean isChallenge = false;
-                try {
-                    //2.使用meta-data
-                    ApplicationInfo applicationInfo = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
-                    isChallenge = applicationInfo.metaData.getBoolean("isChallenge");
-                } catch (PackageManager.NameNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                if (isChallenge) {
-                    //1.自定义Toast
-                    Toast toast = Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
-                    toast.setGravity(Gravity.TOP, 0, 0);
-                    toast.show();
-                } else {
-                    Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
-                }
+                checkAnswer(true);
             }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        updateQuestion();
+    }
+
+    private void updateQuestion() {
+        mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId());
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        if (userPressedTrue == mQuestionBank[mCurrentIndex].isAnswerTrue())
+            Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
     }
 }
